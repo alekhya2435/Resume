e2907b8cc7525db5300f5334f4f36ab1b538d785
Alekhya <kotloalekhyareddy@gmail.com>
2025-09-16 12:10:28 +0530

    first commit

--- my-counter-app/src/App.jsx
+++ my-counter-app/src/App.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import './App.css';
-function Count(){
-const[count,setCount]=useState(0)
-return(<div className="App">
-<h2>Counter:{count}</h2>
-<button onClick={()=>setCount(count-1)}>Decrement</button>
-<button onClick={()=>setCount(count-1)}>Iecrement</button>
 
-</div>)
+function Count() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: count >= 5 ? "orange" : "white",
+        minHeight: "100vh", // make sure background covers full screen
+        padding: "20px",
+      }}
+    >
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
 }
-export default Count
+
+export default Count;
