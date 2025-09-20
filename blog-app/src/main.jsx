050c2eb423a0c143fd705a06acb4b8d4b9e380eb
Alekhya <kotloalekhyareddy@gmail.com>
2025-09-20 13:19:10 +0530

    first commit

--- blog-app/src/main.jsx
+++ blog-app/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css';
-import F1 from './f1.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <F1 />
-  </StrictMode>,
-);
-export default F1;
+// index.js or main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from'./App.jsx';
+import F1 from './f1.jsx'; // F1 is your main component (probably App)
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
